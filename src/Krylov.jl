b9733fa8698ad4dc3ff14e1315cfceba2450e4a3
JayRen <jieren3996@foxmail.com>
2021-03-04 00:09:06 +0800

    Update Krylov.jl

--- src/Krylov.jl
+++ src/Krylov.jl
@@ -3,14 +3,17 @@
 #---------------------------------------------------------------------------------------------------
 import LinearAlgebra: mul!
 import Base: *, eltype
+export kraus
 struct Kraus{TL, TU}
     KL::Array{TL, 3}
     KU::Array{TU, 3}
     dir::Symbol
 end
 eltype(::Kraus{TL, TU}) where TL where TU = promote_type(TL, TU)
+kraus(KL, KR; dir::Symbol=:r) = Kraus(Array(KL), Array(KR), dir)
+kraus(KL; dir::Symbol=:r) = Kraus(Array(KL), conj.(KL), dir)
 #---------------------------------------------------------------------------------------------------
-function mul!(ρ::AbstractMatrix, k::Kraus, ρ0::AbstractMatrix)
+function mul!(ρ::Matrix, k::Kraus, ρ0::AbstractMatrix)
     KL, KU, dir = k.KL, k.KU, k.dir
     if dir == :r
         @tensor ρ[:] = KL[-1, 3, 1] * ρ0[1, 2] * KU[-2, 3, 2]
@@ -20,11 +23,18 @@ function mul!(ρ::AbstractMatrix, k::Kraus, ρ0::AbstractMatrix)
         error("Illegal direction: $dir.")
     end
 end
+
+function mul!(ρ::Vector, k::Kraus, ρ0::AbstractVector)
+    α = size(k.KL, 1)
+    ρ_r = reshape(ρ, α, α)
+    ρ0_r = reshape(ρ0, α, α)
+    mul!(ρ_r, k, ρ0_r)
+end
 #---------------------------------------------------------------------------------------------------
-function *(k::Kraus, ρ0::AbstractMatrix)
+function *(k::Kraus, ρ0::AbstractVecOrMat)
     ctype = promote_type(eltype.((k, ρ0))...)
-    ρ = Matrix{ctype}(undef, size(ρ0))
-    mul!(ρ, K, ρ0)
+    ρ = Array{ctype}(undef, size(ρ0))
+    mul!(ρ, k, ρ0)
 end
 
 #---------------------------------------------------------------------------------------------------
